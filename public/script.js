4749345a535fc1a00daa44d8cbb78bddfcdb778f
believe <believeosawaru2@gmail.com>
2024-05-31 15:48:50 +0100

    nb

---
+++ public/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu');
+
+function openMenu() {
+    document.querySelector('.menu').classList.add('menu-transit')
+}
+
+function closeMenu() {
+    document.querySelector('.menu').classList.remove('menu-transit');
+}
