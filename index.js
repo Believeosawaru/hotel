8d88639b9a114a1b37c06941799a6ed163b4feed
believe <believeosawaru2@gmail.com>
2024-05-31 14:17:40 +0100

    bb

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const route = require('./routes/routes')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express();
 
