dabeacefad7fad1e4ef7fe0a10b73358ea4b5bc6
believe <believeosawaru2@gmail.com>
2024-06-06 18:02:31 +0100

    jj

--- public/script.js
+++ public/script.js
@@ -7,3 +7,22 @@ function openMenu() {
 function closeMenu() {
     document.querySelector('.menu').classList.remove('menu-transit');
 }
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.querySelector('.scroller').classList.add('scroller-transit')
+    } else {
+        document.querySelector('.scroller').classList.remove('scroller-transit')
+    }
+}
+
+window.onscroll = () => {
+    scrollFunction();
+}
+
+function headToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
